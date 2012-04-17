722e14b34c354984a38b8f0070af82ca3fc3197f
Zolzaya Erdenebaatar <zolzaya.erdenebaatar@gmail.com>
2012-04-17 21:51:26 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Limnux::Application.config.secret_token = '758d8a8d5b3ccba858fd2015a80c40f9e0fb174ea86d39c0eb1b1b4755b2a916c646a6d7cb08e1e161c0d22b747866b79ab702aa9a620be14ce8ccbb1468dad0'
